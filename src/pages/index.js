adc3773bcc1183d3a97f8ecf880b247fdf7089db
John Almazora <jalmazora.dev@gmail.com>
2021-06-12 22:56:30 -0700

    Refactor SEO to remove pascal case warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,46 @@
 import React from "react"
 import Layout from "../layout/layout"
-import SEO from '../components/seo'
+import Helmet from "../components/seo"
 
 export default function Home() {
   return (
     <Layout>
-      <SEO title='Home' />
-      <div className="relative pt-64 pb-64 flex content-center items-center justify-center">
+      <Helmet title="Home" />
+      <div className="relative flex items-center content-center justify-center pt-64 pb-64">
         <div className="absolute top-0 w-full h-full bg-center bg-cover">
-          <span className="w-full h-full absolute bg-black"></span>
+          <span className="absolute w-full h-full bg-black"></span>
         </div>
         <div className="container relative mx-auto" data-aos="fade-in">
-          <div className="items-center flex flex-wrap">
-            <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+          <div className="flex flex-wrap items-center">
+            <div className="w-full px-4 ml-auto mr-auto text-center lg:w-6/12">
               <div>
-                <h1 className="text-white font-semibold text-5xl">
+                <h1 className="text-5xl font-semibold text-white">
                   <span className="text-purple-500">Gatsby</span> Starter
                 </h1>
                 <p className="mt-4 text-lg text-gray-300">
-                  A simple starter pack that ships with Tailwind CSS, SASS, Blog page using Markdown, SEO and more
+                  A simple starter pack that ships with Tailwind CSS, SASS, Blog
+                  page using Markdown, SEO and more
                 </p>
-                <a href='https://www.github.com/j-almazora/gatsby-tailwind-starter'
-                  className="mt-5 bg-transparent hover:bg-purple-500 text-purple-500 font-semibold hover:text-white p-4 px-6 border border-purple-500 hover:border-transparent rounded inline-block">
+                <a
+                  href="https://www.github.com/j-almazora/gatsby-tailwind-starter"
+                  className="inline-block p-4 px-6 mt-5 font-semibold text-purple-500 bg-transparent border border-purple-500 rounded hover:bg-purple-500 hover:text-white hover:border-transparent"
+                >
                   Download
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden">
-          <svg className="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"
-            version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+        <div className="absolute bottom-0 left-0 right-0 top-auto w-full overflow-hidden pointer-events-none">
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
             <polygon points="2560 0 2560 100 0 100"></polygon>
           </svg>
         </div>
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,10 @@
 import { graphql } from "gatsby"
 import React from "react"
-import SEO from "../components/seo"
+import Helmet from "../components/seo"
 import Layout from "../layout/layout"
 
 export const query = graphql`
-  query($slug: String!) {
+  query ($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
@@ -19,15 +19,17 @@ export const query = graphql`
 const Blog = props => {
   const { title, date } = props.data.markdownRemark.frontmatter
   const { timeToRead } = props.data.markdownRemark
-  
+
   return (
     <Layout>
-      <SEO title={title} />
+      <Helmet title={title} />
       <div className="container mx-auto text-white">
         <h1>{title}</h1>
         <p>{date}</p>
-        <p>{`${timeToRead} ${timeToRead > 1 ? 'mins' : 'min'} read`}</p>
-        <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+        <p>{`${timeToRead} ${timeToRead > 1 ? "mins" : "min"} read`}</p>
+        <div
+          dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+        ></div>
       </div>
     </Layout>
   )
